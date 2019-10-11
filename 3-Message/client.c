79c9abb8532be3533630d0c430082a30aac2555d
Uenn <k16026ku@apps.kct.ac.jp>
2019-10-11 22:47:40 +0900

    10/11

--- 3-Message/client.c
+++ 3-Message/client.c
@@ -18,9 +18,23 @@ int main(int argc, char **argv[])
         DieWithError("usage: client ./client ip_address port");
     int sock
 
-        void
-        DieWithError(char *errorMesseage)
+        void DieWithError(char *errorMesseage)
     {
         perror(errorMessage);
         exit(1);
     }
+ int prepare_client_socket(char *ipaddr, int port){
+     int sock = socket(PF_INET, SOCK_STREAM, 0 );
+     if (sock < 0)
+        DieWithError("socket() failed");
+
+    struct sockaddr_in target;
+    target.sin_familly = AF_INET;
+    target.sin_addr.s_addr = inet_addr(ipaddr);
+    target.sin_port = htons(port);
+    if (connect(sock, (struct sockaddr*)&target, sizeof(target) < 0)
+        DieWithError("connect() failed");
+    
+    return sock;
+
+ }
